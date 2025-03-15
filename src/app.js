8f70d0bba0422a5eb598291aa3724b9485e02eca
Felipe Arce <arceprogramando@gmail.com>
2025-03-15 02:14:42 -0300

    test: test husky pre-commit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,12 @@ import createFolderWithComment from './service/folder.service.js';
 import fetchNames from './dao/fetchUser.dao.js';
 import getFirstThreeElements from './utils/firstThreeElements.utils.js';
 
+const setError = (error) => error;
 fetchNames()
   .then((names) => {
     const firstThreeNames = getFirstThreeElements(names);
     firstThreeNames.forEach((name) => createFolderWithComment(name));
   })
   .catch((error) => {
-    console.error('Error:', error);
+    setError(error);
   });
